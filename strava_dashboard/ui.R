e367c15845292add5ae2d118216a653f75e93f66
samanthacsik <samanthacsik@gmail.com>
2023-01-09 17:15:49 -0800

    added tabsetPanel to dashboard menuitem

--- strava_dashboard/ui.R
+++ strava_dashboard/ui.R
@@ -89,8 +89,7 @@ body <- dashboardBody(
 
       # fluidRow with footer ----
       fluidRow(
-        includeMarkdown("text/home_page_footer.md")
-      ) # END fluidRow
+        includeMarkdown("text/home_page_footer.md")) # END fluidRow
 
     ), # END "about" tabItem
 
@@ -98,114 +97,133 @@ body <- dashboardBody(
     tabItem(
       tabName = "dashboard",
 
-      # style valueBox colors (tried moving this to styles.css but haven't got it working yet)
-      tags$style(".small-box.bg-orange { background-color: #b35702 !important; color: #FFFFFF !important; }"),
-      tags$style(".small-box.bg-purple { background-color: #744082 !important; color: #FFFFFF !important; }"),
-      tags$style(".small-box.bg-green { background-color: #366643 !important; color: #FFFFFF !important; }"),
-      tags$style(".small-box.bg-black { background-color: #64605f !important; color: #FFFFFF !important; }"),
+      # dashboard tabsetPanel ----
+      tabsetPanel(
+        id = "dashboard_tabsetPanel",
+
+        # map tabPanel ----
+        tabPanel(
+          title = "Gear Tracker & Heat Map",
+
+          # style valueBox colors (tried moving this to styles.css but haven't got it working yet)
+          tags$style(".small-box.bg-orange { background-color: #b35702 !important; color: #FFFFFF !important; }"),
+          tags$style(".small-box.bg-purple { background-color: #744082 !important; color: #FFFFFF !important; }"),
+          tags$style(".small-box.bg-green { background-color: #366643 !important; color: #FFFFFF !important; }"),
+          tags$style(".small-box.bg-black { background-color: #64605f !important; color: #FFFFFF !important; }"),
 
 
-      # style box header colors
-      # tags$style(".box.box-solid.box-primary>.box-header {background-color: #98A08D!important; } color: #FFFFFF !important; }"),
+          # style box header colors
+          # tags$style(".box.box-solid.box-primary>.box-header {background-color: #98A08D!important; } color: #FFFFFF !important; }"),
 
-      # first row  (contains gear garage & leaflet boxes)----
-      fluidRow(
+          # first row  (contains gear garage & leaflet boxes)----
+          fluidRow(
 
-        # gear garage box ----
-        box(width = 4, # height = 930, (setting height causes problems with dynamic screen size)
-            span(
-              tags$div(includeMarkdown("text/gear_garage.md"))
-            ), # END span
+            # gear garage box ----
+            box(width = 4, # height = 930, (setting height causes problems with dynamic screen size)
+                span(
+                  tags$div(includeMarkdown("text/gear_garage.md"))
+                ), # END span
 
-            # gear_shoes pickerInput ----
-            pickerInput(inputId = "gear_shoes", label = "Select shoes:",
-                        choices = c("Danner Jag #3" = "danner3", "Danner Jag #2" = "danner2", "Danner Jag #1 (retired)" = "danner1",
-                                    "Lowa Renegade" = "lowa", "Reebok Nano (black/white)" = "nano"),
-                        selected = "Danner Jag #3",
-                        multiple = FALSE), # END gear pickerInput
+                # gear_shoes pickerInput ----
+                pickerInput(inputId = "gear_shoes", label = "Select shoes:",
+                            choices = c("Danner Jag #3" = "danner3", "Danner Jag #2" = "danner2", "Danner Jag #1 (retired)" = "danner1",
+                                        "Lowa Renegade" = "lowa", "Reebok Nano (black/white)" = "nano"),
+                            selected = "Danner Jag #3",
+                            multiple = FALSE), # END gear pickerInput
 
-            # shoes mileage infoBoxOutput ----
-            valueBoxOutput(outputId = "gear_shoes_mileage", width = 12) |> withSpinner(color = "#cb9e72", type = 1),
+                # shoes mileage infoBoxOutput ----
+                valueBoxOutput(outputId = "gear_shoes_mileage", width = 12) |> withSpinner(color = "#cb9e72", type = 1),
 
-            # gear_bike pickerInput ----
-            pickerInput(inputId = "gear_bike", label = "Select bike:",
-                        choices = c("Canyon Grail 7 (gravel)" = "grail", "Tandemania (tandem road bike)" = "tandem", "Yeti (MTB)" = "yeti",
-                                    "Trek (MTB)" = "trek", "Cannondale (MTB)" = "cannondale"),
-                        selected = "Canyon Grail 7 (gravel)",
-                        multiple = FALSE), # END gear pickerInput
+                # gear_bike pickerInput ----
+                pickerInput(inputId = "gear_bike", label = "Select bike:",
+                            choices = c("Canyon Grail 7 (gravel)" = "grail", "Tandemania (tandem road bike)" = "tandem", "Yeti (MTB)" = "yeti",
+                                        "Trek (MTB)" = "trek", "Cannondale (MTB)" = "cannondale"),
+                            selected = "Canyon Grail 7 (gravel)",
+                            multiple = FALSE), # END gear pickerInput
 
 
-            # bike mileage infoBoxOutput ----
-            valueBoxOutput(outputId = "gear_bike_mileage", width = 12) |> withSpinner(color = "#cb9e72", type = 1),
+                # bike mileage infoBoxOutput ----
+                valueBoxOutput(outputId = "gear_bike_mileage", width = 12) |> withSpinner(color = "#cb9e72", type = 1),
 
-            # photo of boots ----
-            tags$img(class = "banner",
-                     src = "media/danner.jpeg"),
+                # photo of boots ----
+                tags$img(class = "banner",
+                         src = "media/danner.jpeg"),
 
-            # danner description ----
-            span(
-              tags$div(includeMarkdown("text/danner.md"))
-            )
-          ), # END gear garage box
+                # danner description ----
+                span(
+                  tags$div(includeMarkdown("text/danner.md")))
 
-        # leaflet box ----
-        box(width = 8, # height = 930,
+            ), # END gear garage box
 
-            # leaflet box header text ----
-            span(
-              tags$div(includeMarkdown("text/leaflet_info.md"))
-            ), # END span
+            # leaflet box ----
+            box(width = 8, # height = 930,
 
-            # value boxes, sliderInputs, leaflet map ----
-            valueBoxOutput(outputId = "total_filtered_hikes"),
-            valueBoxOutput(outputId = "total_filtered_rides"),
-            valueBoxOutput(outputId = "total_filtered_walks"),
-            sliderInput(inputId = "distance_sliderInput", label = "Select a distance (miles) range:",
-                        min = min(acts$total_miles), max = max(acts$total_miles),
-                        value = c(min(acts$total_miles), max(acts$total_miles))),
-            sliderInput(inputId = "elevation_sliderInput", label = "Select a range of elevation gain (ft):",
-                        min = min(acts$elevation_gain_ft), max = max(acts$elevation_gain_ft),
-                        value = c(min(acts$elevation_gain_ft), max(acts$elevation_gain_ft))),
-            leafletOutput(outputId = "strava_map") |> withSpinner(color = "#cb9e72", type = 1)
+                # leaflet box header text ----
+                span(
+                  tags$div(includeMarkdown("text/leaflet_info.md"))
+                ), # END span
+
+                # value boxes, sliderInputs, leaflet map ----
+                valueBoxOutput(outputId = "total_filtered_hikes"),
+                valueBoxOutput(outputId = "total_filtered_rides"),
+                valueBoxOutput(outputId = "total_filtered_walks"),
+                sliderInput(inputId = "distance_sliderInput", label = "Select a distance (miles) range:",
+                            min = min(acts$total_miles), max = max(acts$total_miles),
+                            value = c(min(acts$total_miles), max(acts$total_miles))),
+                sliderInput(inputId = "elevation_sliderInput", label = "Select a range of elevation gain (ft):",
+                            min = min(acts$elevation_gain_ft), max = max(acts$elevation_gain_ft),
+                            value = c(min(acts$elevation_gain_ft), max(acts$elevation_gain_ft))),
+                leafletOutput(outputId = "strava_map") |> withSpinner(color = "#cb9e72", type = 1)
+
             ) # END leaflet box
-      ), # END first fluidRow
 
-      # add some space between map and plots ----
-      headerPanel(""),
+          ) # END first fluidRow
 
-      # second fluid row (contains distance & elevation plots) ----
-      fluidRow(
-        tabBox(width = 12,
-               title = tags$strong("Explore distance and elevation stats"),
-               side = "right", selected = "Distance & Elevation Summary Stats",
+        ), # END map tabPanel ----
 
-               # elev ~ dist scatterplot ----
-               tabPanel("Elevation Gain / Distance Scatterplot",
-                        sport_type_pickerInput(inputId = "sport_scatterplot"), # sport type input
-                        date_range_airDatepickerInput(inputId = "date_scatterplot"), # date range input
-                        plotly::plotlyOutput(outputId = "elev_dist_scatterplot") |> withSpinner(color = "#cb9e72", type = 1) # elev ~ dist scatterplot output
-               ), # END tabPanel (scatterplot)
+        # dist & elev tabPanel ----
+        tabPanel(
+          title = "Distance & Elevation Stats",
 
-               # dist & elev histograms ----
-               tabPanel("Distance & Elevation Summary Stats",
-                        sport_type_pickerInput(inputId = "sport_histogram"), # sport type input
-                        date_range_airDatepickerInput(inputId = "date_histogram"), # date range input
-                        splitLayout(cellWidths = c("50%", "50%"), # histogram outputs
-                                    plotOutput(outputId = "dist_histogram") |> withSpinner(color = "#cb9e72", type = 1),
-                                    plotOutput(outputId = "elev_histogram") |> withSpinner(color = "#cb9e72", type = 1)),
-                        headerPanel(""),
-                        headerPanel(""),
-                        tableOutput(outputId = "dist_elev_stats_table")
-               ), # END tabPanel (histograms)
+          # dist $ elev fluidRow ----
+          fluidRow(
 
-              ) # END tabBox
-      ), # END second fluidRow
+            tabBox(width = 12,
+                   title = tags$strong("Explore distance and elevation stats"),
+                   side = "right", selected = "Distance & Elevation Summary Stats",
 
-      # fluidRow cluster analysis ----
-      # fluidRow(
-      #   box(width = 12,
-      #       title = tags$strong("Kmeans Cluster Analysis"))
-      # ) # END fluidRow
+                   # elev ~ dist scatterplot ----
+                   tabPanel("Elevation Gain / Distance Scatterplot",
+                            sport_type_pickerInput(inputId = "sport_scatterplot"), # sport type input
+                            date_range_airDatepickerInput(inputId = "date_scatterplot"), # date range input
+                            plotly::plotlyOutput(outputId = "elev_dist_scatterplot") |> withSpinner(color = "#cb9e72", type = 1) # elev ~ dist scatterplot output
+                   ), # END tabPanel (scatterplot)
+
+                   # dist & elev histograms ----
+                   tabPanel("Distance & Elevation Summary Stats",
+                            sport_type_pickerInput(inputId = "sport_histogram"), # sport type input
+                            date_range_airDatepickerInput(inputId = "date_histogram"), # date range input
+                            splitLayout(cellWidths = c("50%", "50%"), # histogram outputs
+                                        plotOutput(outputId = "dist_histogram") |> withSpinner(color = "#cb9e72", type = 1),
+                                        plotOutput(outputId = "elev_histogram") |> withSpinner(color = "#cb9e72", type = 1)),
+                            headerPanel(""),
+                            headerPanel(""),
+                            tableOutput(outputId = "dist_elev_stats_table")
+                   ), # END tabPanel (histograms)
+
+            ) # END tabBox
+
+          ) # END dist & elev fluidRow
+
+          # fluidRow cluster analysis ----
+          # fluidRow(
+          #   box(width = 12,
+          #       title = tags$strong("Kmeans Cluster Analysis"))
+          # ) # END fluidRow
+
+        ) # END dist & elev tabPanel
+
+      ) # END dashboard tabsetPanels
 
     ), # END "dashboard" tab
 
