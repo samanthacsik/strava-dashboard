2f7a4a11561f9452be90a6bc4e91b63d0941fd98
samanthacsik <samanthacsik@gmail.com>
2023-07-17 10:39:27 -0700

    updated data

--- strava_dashboard/global.R
+++ strava_dashboard/global.R
@@ -22,7 +22,7 @@ library(fresh)
 # library(reactlog)
 
 # IMPORT DATA ----
-acts <- readRDS("data/strava_acts_2023-06-19.rds")
+acts <- readRDS("data/strava_acts_2023-07-17.rds")
 
 # GGPLOT THEME ----
 stravaTheme <- theme_light() +
