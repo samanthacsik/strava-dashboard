c2264cb001debff61e0cee6f5bef1a5680b03d65
samanthacsik <samanthacsik@gmail.com>
2024-06-22 22:33:49 -0600

    updated with latest pagosa data, but most recent gravel ride not appearing on map; figure this out later

--- strava_dashboard/R/shoes_pickerInput.R
+++ strava_dashboard/R/shoes_pickerInput.R
@@ -2,9 +2,13 @@ shoes_pickerInput <- function(inputId) {
 
   pickerInput(inputId = inputId, label = "Select shoes:",
               choices = c("Hoka Speedgoat #1" = "hoka1",
-                          "Danner Jag #4" = "danner4", "Lowa Renegade" = "lowa",
+                          "Hoka Speedgoat #2" = "hoka2",
+                          "Teva Hurricane XLT2" = "tevas",
+                          "Danner Jag #4" = "danner4",
+                          "Lowa Renegade" = "lowa",
                           "Danner Jag #3 (retired)" = "danner3",
-                          "Danner Jag #2 (retired)" = "danner2", "Danner Jag #1 (retired)" = "danner1"), #"Reebok Nano" = "nano"
+                          "Danner Jag #2 (retired)" = "danner2",
+                          "Danner Jag #1 (retired)" = "danner1"), #"Reebok Nano" = "nano"
               selected = "Hoka Speedgoat",
               multiple = FALSE)
 
