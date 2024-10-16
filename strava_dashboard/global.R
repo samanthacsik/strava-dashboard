6faff561b064f72ee232de044acfde3b0f23fba8
samanthacsik <samanthacsik@gmail.com>
2024-10-16 15:54:09 -0700

    write to aws in scrape_strava.qmd; removed old commented out lines in global.R

--- strava_dashboard/global.R
+++ strava_dashboard/global.R
@@ -32,15 +32,3 @@ stravaTheme <- theme_light() +
         legend.text = element_text(size = 13),
         panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.7),
         plot.caption = element_text(size = 10, hjust = 0))
-
-# STYLING ----
-# hike_color <- "#b35702" # orange
-# bike_color <- "#744082" # purple
-# walk_color <- "#366643" # green
-
-# SPINNER STYLING (NOT WORKING) ----
-# options(spinner.type = 6, spinner.size = 2 , spinner.color = "#cb9e72")
-
-# SLIDESHOW IMAGES ----
-# imgs <- list.files("strava_dashboard/www/slideshow_photos", pattern = ".jpeg", full.names = TRUE)
-
