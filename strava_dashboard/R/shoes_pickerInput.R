226fe5f5a7332ae3b98fe41b09c8eb176b8c15a0
samanthacsik <samanthacsik@gmail.com>
2025-03-31 11:23:43 -0700

    updated data

--- strava_dashboard/R/shoes_pickerInput.R
+++ strava_dashboard/R/shoes_pickerInput.R
@@ -2,8 +2,9 @@ shoes_pickerInput <- function(inputId) {
 
   pickerInput(inputId = inputId,
               label = "Select shoes:",
-              choices = c("Hoka Speedgoat #1" = "hoka1",
-                          "Hoka Speedgoat #2" = "hoka2",
+              choices = c("Hoka Speedgoat 5 (#1; retired)" = "hoka1",
+                          "Hoka Speedgoat 5 (#2)" = "hoka2",
+                          "Hoka Speedgoat 6 (#3)" = "hoka3",
                           "Teva Hurricane XLT2" = "tevas",
                           "Danner Jag #4" = "danner4",
                           "Lowa Renegade" = "lowa",
